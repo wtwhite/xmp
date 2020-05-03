210445d0d1afa9b5270c6d813b348338a461a6d0
wtwhite <github.20.J_Random_Hacker@spamgourmet.com>
2020-05-03 19:50:30 +0000

    Revert "Log all calls to Arena...() functions."
    This reverts commit 67b24369fb6921ae730c2f02293e9f6437a37c90.

--- src/arena.h
+++ src/arena.h
@@ -10,7 +10,6 @@
 #include <string.h>
 #include "switches.h"		// Needed for BYTESPERBLOCK
 #include "alignedalloc.h"	// Needed for ALIGNED_MALLOC(), ALIGNED_FREE()
-#include <dbgprint.h>
 
 struct arena {
 	unsigned char *cur;
@@ -22,7 +21,6 @@ struct arena {
 // or blank (in the increasingly unlikely event that it is not).
 
 static INLINE void ArenaCreate(struct arena *a, size_t size) {
-	DBGPRINT3("ArenaCreate(a=%p, size=%u) called.\n", a, size);
 	assert(size % BYTESPERBLOCK == 0);
 	ALIGNED_MALLOC(a->base, size);
 	assert((uintptr_t) a->base % BYTESPERBLOCK == 0);
@@ -31,13 +29,11 @@ static INLINE void ArenaCreate(struct arena *a, size_t size) {
 }
 
 static INLINE void ArenaDestroy(struct arena *a) {
-	DBGPRINT2("ArenaDestroy(a=%p) called.\n", a);
 	ALIGNED_FREE(a->base);
 }
 
 static INLINE void *ArenaAllocate(struct arena *a, size_t size) {
 	unsigned char *p = a->cur;
-	DBGPRINT3("ArenaAllocate(a=%p, size=%u) called.\n", a, size);
 	assert(size % BYTESPERBLOCK == 0);
 	assert(a->cur + size <= a->base + a->size);
 	a->cur += size;
@@ -51,12 +47,10 @@ static INLINE void *ArenaAllocate(struct arena *a, size_t size) {
 }
 
 static INLINE void *ArenaGetPos(struct arena *a) {
-	DBGPRINT3("ArenaGetPos(a=%p) called, returning %p.\n", a, a->cur);
 	return a->cur;
 }
 
 static INLINE void ArenaSetPos(struct arena *a, void *p) {
-	DBGPRINT3("ArenaSetPos(a=%p, p=%p) called.\n", a, p);
 	assert((unsigned char *) p >= a->base && (unsigned char *) p <= a->base + a->size);
 	assert((uintptr_t) p % BYTESPERBLOCK == 0);
 	a->cur = p;
